bd0d0c10d0b35a5d3ca4d5fa2adcc88894c4df26
Rishi Mody <67372250+rishimody13@users.noreply.github.com>
2023-11-02 23:51:19 +0530

    update graphing

--- stockcode.py
+++ stockcode.py
@@ -1,11 +1,12 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from yahoo_fin import stock_info as si
 # Define the list of stocks to collect data for
 
 
-stocks = ['AAPL'] # Insert list of symbols to analyze
+stocks = ['AAPL', 'TSLA', 'MSFT', 'GOOGL','NVDA', 'META', 'NFLX', 'INTC', 'AMZN'] # Insert list of symbols to analyze
+
 
 
 
@@ -18,20 +19,20 @@ for stock in stocks:
    
     #Collect data using yfinance
     stock_price = yf.Ticker(stock).history(period="1d")["Close"][0]
-    historical_data = yf.Ticker(stock).history(period="1mo")
+    historical_data = yf.Ticker(stock).history(period="1y")
     start_price = historical_data["Close"][0]
     end_price = historical_data["Close"][-1]
     momentum = end_price - start_price
     # momentum = stock_info['momentum']
     # market_cap = (stock_info['marketCap'])/1000000000
     
-    # pe_ratio = stock_info["trailingPE"]
-    current_price = stock.history(period="1d")["Close"][0]
-    # Estimate the EPS using historical data (e.g., last 4 quarters)
-    historical_data = stock.history(period="1y")
-    average_earnings = historical_data["Close"].mean()
-    # Calculate the P/E ratio
-    pe_ratio = current_price / average_earnings
+    pe_ratio = si.get_quote_table(stock)["PE Ratio (TTM)"]
+    # current_price = yf.Ticker(stock).history(period="1d")["Close"][0]
+    # # Estimate the EPS using historical data (e.g., last 4 quarters)
+    # historical_data = yf.Ticker(stock).history(period="1y")
+    # average_earnings = historical_data["Close"].mean()
+    # # Calculate the P/E ratio
+    # pe_ratio = current_price / average_earnings
     
 
     
@@ -48,25 +49,39 @@ stock_data.to_excel('gitstockdata.xlsx', index=False)
 #GRAPHING
 
 # Generate a graph of the stock price over 5 months for each stock
+# for stock in stocks:
+#     # Collect the data using yfinance
+#     stock_df = yf.download(stock, period='5mo', interval='1d')
+    
+#     # Plot the data
+#     plt.plot(stock_df['Close'], label=stock)
+    
+# # Add labels and legend to the graph
+# plt.title('Stock Price over 5 Months')
+# plt.xlabel('Date')
+# plt.ylabel('Price')
+# plt.legend()
+
+# # Show the graph
+# plt.show()
+
+
+#generate graphs for percentage change
 for stock in stocks:
     # Collect the data using yfinance
-    stock_df = yf.download(stock, period='5mo', interval='1d')
+    stock_df = yf.download(stock, period='6mo', interval='1d')
     
-    # Plot the data
-    plt.plot(stock_df['Close'], label=stock)
+    # Calculate the percentage change
+    stock_df['Percentage Change'] = (stock_df['Close'] - stock_df['Close'].iloc[0]) / stock_df['Close'].iloc[0] * 100
     
+    # Plot the percentage change
+    plt.plot(stock_df.index, stock_df['Percentage Change'], label=stock)
+
 # Add labels and legend to the graph
-plt.title('Stock Price over 5 Months')
+plt.title('Percentage Price Change Over 6 months')
 plt.xlabel('Date')
-plt.ylabel('Price')
+plt.ylabel('Percentage Change')
 plt.legend()
 
 # Show the graph
 plt.show()
-
-
-#     # print(f"P/E Ratio: {pe_ratio}")
-#     # print(f"Market Cap: {market_cap}")
-
-# # Note: Be sure to replace 'YOUR_API_KEY' with your actual Alpha Vantage API key.
-# stock_data.to_excel('gitstockdata.xlsx', index=False)
