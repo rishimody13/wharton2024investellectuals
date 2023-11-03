ed80d5d53f8bea58d6682b66660cfcac2cbcc782
Rishi Mody <rishimody13@hotmail.com>
2023-11-03 15:38:28 +0530

    edit pe ratio code

--- stockcode.py
+++ stockcode.py
@@ -2,11 +2,11 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from yahoo_fin import stock_info as si
+import time
 # Define the list of stocks to collect data for
 
 
-stocks = ['SPY', 'IVV', 'VOO', 'VTI', 'QQQ', 'VTV', 'VUG', 'IWF', 'VIG', 'IJH', 'IJR']
-
+stocks = ['IVV', 'VOO', 'VTI', 'QQQ', 'VTV', 'VUG', 'IWF', 'VIG', 'IJH', 'IJR']
 #stocks = ['AAPL', 'TSLA', 'MSFT', 'GOOGL','NVDA', 'META', 'NFLX', 'INTC', 'AMZN'] # Insert list of symbols to analyze
 
 
@@ -18,32 +18,50 @@ stock_data = pd.DataFrame(columns=['Stock', 'Price', 'Momentum','PE Ratio'])
 
 # Loop through each stock and collect the data
 for stock in stocks:
-   
+    
     #Collect data using yfinance
     stock_price = yf.Ticker(stock).history(period="1d")["Close"][0]
     historical_data = yf.Ticker(stock).history(period="1y")
     start_price = historical_data["Close"][0]
     end_price = historical_data["Close"][-1]
     momentum = end_price - start_price
-    # momentum = stock_info['momentum']
-    # market_cap = (stock_info['marketCap'])/1000000000
-    
-    pe_ratio = si.get_quote_table(stock)["PE Ratio (TTM)"]
-    # current_price = yf.Ticker(stock).history(period="1d")["Close"][0]
-    # # Estimate the EPS using historical data (e.g., last 4 quarters)
-    # historical_data = yf.Ticker(stock).history(period="1y")
-    # average_earnings = historical_data["Close"].mean()
-    # # Calculate the P/E ratio
-    # pe_ratio = current_price / average_earnings
-    
 
+    # MCAP -> market_cap = (stock_info['marketCap'])/1000000000
     
+    
+    # PE RATIO -> quote = si.get_quote_table(stock)
+    # print(quote)
+    # pe_ratio = quote["PE Ratio (TTM)"]
+    
+    attempt = 1
+    time.sleep(3)
+    error = False
+    try:
+        val = si.get_stats_valuation(stock)
+        print(f"{stock} loaded on first attempt")
+    except:
+        error = True
+        attempt += 1
+        while error == True:
+            print(f"Sleeping {attempt*10}s")
+            time.sleep(attempt*10)
+            try:
+                x = si.get_stats_valuation(stock)
+                error = False
+                print(f"{stock}, succesful on attempt {attempt}")
+            except:
+                attempt += 1
+                print(f"{stock}, failed. Attempt {attempt}") 
+    # val = si.get_stats_valuation(stock)
+    val = val.iloc[:,:2]
+    val.columns = ["Attribute", "Recent"]
+    pe_ratio = float(val[val.Attribute.str.contains("Trailing P/E")].iloc[0,1])
+     
 
     # Add the data to the dataframe
     
     stock_data = stock_data.append({'Stock': stock, 'Price': stock_price,  'Momentum': momentum, 'PE Ratio': pe_ratio}, ignore_index=True)
-# 'PE Ratio': pe_ratio
-# 'Market Cap (Billions)': market_cap
+
        
 # Export the data to an excel sheet
 stock_data.to_excel('stockdata.xlsx', index=False)
@@ -89,3 +107,5 @@ def sixmonthpercentagechange():
 
     # Show the graph
     plt.show()
+
+fivemonthpricechange()
